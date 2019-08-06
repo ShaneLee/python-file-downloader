6789324acc26e348cadfcc9e99ff46e971671ad5
Shane Lee <Shane@Shanes.local>
2019-08-06 18:22:53 +0100

    Fixed errors

--- grabfiles.py
+++ grabfiles.py
@@ -1,18 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 from requests import get
-page = requests.get(raw_input("Enter URL: "))
+
+page = requests.get("https://www.cbc.ca/radio/podcasts/current-affairs-information/uncover/")
 html = page.text
 soup = BeautifulSoup(html, "html.parser")
-body = soup.find("div", attrs={'class': 'sclt-feed-uncover'})
+body = soup.find("div", attrs={'class': 'sclt-feed-uncover'}).find_all("p")
 
-for link in body.find_all('a'):
-    print(link)
-    url = link.get('href')
-    if ".mp3" in url:
-        print(url)
-        with open(url, "wb") as file:
-            response = get(domain + url)
-            file.write(response.content)
-    else:
-        continue
+filename = ""
+
+for element in body:
+    if element.find("strong"):
+        filename = element.find("strong").text.replace("- ", "").replace(":", "").replace(" ", "_") + ".mp3"
+    if element.find("a"):
+        link = element.find("a")
+        print(filename)
+        url = link.get('href')
+        if ".mp3" in url:
+            print(url)
+            with open(filename, "wb") as file:
+                response = get(url)
+                file.write(response.content)
+        else:
+            continue
