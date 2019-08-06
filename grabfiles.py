00e1b9760242c8b08c36b2baa02405e27d30f105
Shane Lee <Shane@Shanes.local>
2019-08-06 18:09:31 +0100

    Made script more universal

--- grabfiles.py
+++ grabfiles.py
@@ -1,17 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 from requests import get
-domain = "http://purehumbug.com"
-page = requests.get("http://purehumbug.com/shows/2006/1-99/")
+
+page = requests.get(raw_input("Enter URL: "))
+filetype = '.' + raw_input("Enter File Extension (with no dot):")
 html = page.text
 soup = BeautifulSoup(html, "html.parser")
 
 for link in soup.find_all('a'):
     url = link.get('href')
-    if ".mp3" in url:
+    if filetype in url:
         print(url)
-        file_name = url.split("1-99/", 1)[1]
-        with open(file_name, "wb") as file:
+        with open(url, "wb") as file:
             response = get(domain + url)
             file.write(response.content)
     else:
